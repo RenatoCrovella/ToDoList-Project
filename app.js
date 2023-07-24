d94b81415d2fb1f59ea2f4e95127d9637aa652b6
Renato Crovella Leiva <renato.leiva@hotmail.com>
2023-07-24 10:46:12 +0100

    the server is ok

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const express = require("express");
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
 const port = 3000;
 
 app.get("/", (req, res)=>{
-    res.sendFile(__dirname + "/views/today.ejs")
+    res.render(__dirname + "/views/today.ejs")
 })
 
 app.get("/work", (req, res)=>{
@@ -16,6 +19,6 @@ app.get("/buyer", (req, res)=>{
     res.sendFile(__dirname + "/views/buyer.ejs")
 })
 
-app.listen(port, (req, res)=>{
+app.listen(port, ()=>{
     console.log(`server runnning on ${port}`);
 })
